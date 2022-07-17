56a6f070440fea161bae1aa95fa604ea8340e0d2
def-SpaceWar <aryanahire9086360780@gmail.com>
2022-07-16 20:29:01 -0400

    fixking build errors

--- src/quiz.ts
+++ src/quiz.ts
@@ -86,172 +86,172 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   </div>
 `;
 
-setTimeout(() => {  
-  const titles = document.querySelectorAll(".small-title");
-  let cRemoved = 0;
-  const lowerTitleCharacterCount = setInterval(() => {
-    for (let i = 0; i < titles.length; i++) {
-      titles[i].innerHTML = titles[i].innerHTML.slice(0, titles[i].innerHTML.length - 1);
-    }
-    cRemoved++;
-  
-    if (cRemoved >= 5) {
-      clearInterval(lowerTitleCharacterCount);
-    }
-  }, 200);
+setTimeout(() => {
+    const titles = document.querySelectorAll(".small-title");
+    let cRemoved = 0;
+    const lowerTitleCharacterCount = setInterval(() => {
+        for (let i = 0; i < titles.length; i++) {
+            titles[i].innerHTML = titles[i].innerHTML.slice(0, titles[i].innerHTML.length - 1);
+        }
+        cRemoved++;
+
+        if (cRemoved >= 5) {
+            clearInterval(lowerTitleCharacterCount);
+        }
+    }, 200);
 }, 1000)
 
 fadeButtons();
 
 function selectRandomFromArray(array: any[]) {
-  try {
-    const randomIndex = Math.floor(Math.random() * array.length);
-    return array[randomIndex];
-  } catch {
-    return array[array.length - 1];
-  }
+    try {
+        const randomIndex = Math.floor(Math.random() * array.length);
+        return array[randomIndex];
+    } catch {
+        return array[array.length - 1];
+    }
 }
 
 function shuffleArray(arr: any[]) {
-  const newArr = [];
-  
-  for (let i = 0; i < arr.length; i++) {
-    newArr.push(arr[i]);
-  }
+    const newArr = [];
 
-  for (let i = newArr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
-  }
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i]);
+    }
 
-  return newArr;
+    for (let i = newArr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+    }
+
+    return newArr;
 }
 
 let childQuestionCount = 0;
 let adultQuestionCount = 0;
 
 function showQuestion(question: Question) {
-  if (adultQuestionCount < 0) adultQuestionCount = 0;
-  if (childQuestionCount < 0) childQuestionCount = 0;
-  document.querySelector("#parent-points")!.innerHTML = "Points: " + adultQuestionCount.toString();
-  document.querySelector("#child-points")!.innerHTML = "Points: " + childQuestionCount.toString();
-  
-  if (question.adult) {
-    document.querySelector("#parent-question")!.innerHTML = question.question;
-    document.querySelector("#parent-topic")!.innerHTML = question.topic;
-    document.querySelector("#parent-difficulty")!.innerHTML = question.gradeLevel.toString();
+    if (adultQuestionCount < 0) adultQuestionCount = 0;
+    if (childQuestionCount < 0) childQuestionCount = 0;
+    document.querySelector("#parent-points")!.innerHTML = "Points: " + adultQuestionCount.toString();
+    document.querySelector("#child-points")!.innerHTML = "Points: " + childQuestionCount.toString();
 
-    // mix options
-    const newOptions = shuffleArray(question.options);
-    document.querySelector("#parent-option-1")!.innerHTML = newOptions[0];
-    document.querySelector("#parent-option-2")!.innerHTML = newOptions[1];
-    document.querySelector("#parent-option-3")!.innerHTML = newOptions[2];
-    document.querySelector("#parent-option-4")!.innerHTML = newOptions[3];
-  } else {
-    document.querySelector("#child-question")!.innerHTML = question.question;
-    document.querySelector("#child-topic")!.innerHTML = question.topic;
-    
-    switch (question.gradeLevel) {
-      case 0:
-        document.querySelector("#child-difficulty")!.innerHTML = "K";
-        break;
-      default:
-        document.querySelector("#child-difficulty")!.innerHTML = question.gradeLevel.toString();
-        break;
+    if (question.adult) {
+        document.querySelector("#parent-question")!.innerHTML = question.question;
+        document.querySelector("#parent-topic")!.innerHTML = question.topic;
+        document.querySelector("#parent-difficulty")!.innerHTML = question.gradeLevel.toString();
+
+        // mix options
+        const newOptions = shuffleArray(question.options);
+        document.querySelector("#parent-option-1")!.innerHTML = newOptions[0];
+        document.querySelector("#parent-option-2")!.innerHTML = newOptions[1];
+        document.querySelector("#parent-option-3")!.innerHTML = newOptions[2];
+        document.querySelector("#parent-option-4")!.innerHTML = newOptions[3];
+    } else {
+        document.querySelector("#child-question")!.innerHTML = question.question;
+        document.querySelector("#child-topic")!.innerHTML = question.topic;
+
+        switch (question.gradeLevel) {
+            case 0:
+                document.querySelector("#child-difficulty")!.innerHTML = "K";
+                break;
+            default:
+                document.querySelector("#child-difficulty")!.innerHTML = question.gradeLevel.toString();
+                break;
+        }
+
+        // mix options
+        const newOptions = shuffleArray(question.options);
+        document.querySelector("#child-option-1")!.innerHTML = newOptions[0];
+        document.querySelector("#child-option-2")!.innerHTML = newOptions[1];
+        document.querySelector("#child-option-3")!.innerHTML = newOptions[2];
+        document.querySelector("#child-option-4")!.innerHTML = newOptions[3];
     }
-
-    // mix options
-    const newOptions = shuffleArray(question.options);
-    document.querySelector("#child-option-1")!.innerHTML = newOptions[0];
-    document.querySelector("#child-option-2")!.innerHTML = newOptions[1];
-    document.querySelector("#child-option-3")!.innerHTML = newOptions[2];
-    document.querySelector("#child-option-4")!.innerHTML = newOptions[3];
-  }
 }
 
 function selectOption(adult: boolean, optionNum: number): string {
-  return document.querySelector(`#${adult ? "parent" : "child"}-option-${optionNum}`)!.innerHTML;
+    return document.querySelector(`#${adult ? "parent" : "child"}-option-${optionNum}`)!.innerHTML;
 }
 
 function checkAnswer(option: string, question: Question): boolean {
-  if (option == question.options[0]) return true;
-  return false;
+    if (option == question.options[0]) return true;
+    return false;
 }
 
 function runQuiz() {
-  let childQuestion = selectRandomFromArray(questionGroup.filter({ adult: false }));
-  showQuestion(childQuestion);
-  
-  let adultQuestion = selectRandomFromArray(questionGroup.filter({ adult: true }));
-  showQuestion(adultQuestion);
+    let childQuestion = selectRandomFromArray(questionGroup.filter({ adult: false }));
+    showQuestion(childQuestion);
 
-  let finished = false;
-  function updater(e) {
-    if (finished) return;
-    
-    let adult = false;
-    let option = "";
-    
-    if (e.key == "a") {
-      option = selectOption(false, 1);
-    } else if (e.key == "s") {
-      option = selectOption(false, 2);
-    } else if (e.key == "d") {
-      option = selectOption(false, 3);
-    } else if (e.key == "f") {
-      option = selectOption(false, 4);
-    } else if (e.key == "h") {
-      adult = true;
-      option = selectOption(true, 1);
-    } else if (e.key == "j") {
-      adult = true;
-      option = selectOption(true, 2);
-    } else if (e.key == "k") {
-      adult = true;
-      option = selectOption(true, 3);
-    } else if (e.key == "l") {
-      adult = true;
-      option = selectOption(true, 4);
-    }
+    let adultQuestion = selectRandomFromArray(questionGroup.filter({ adult: true }));
+    showQuestion(adultQuestion);
 
-    if (option != "") {
-      if (adult) {
-        if (checkAnswer(option, adultQuestion)) {
-          adultQuestionCount++;
-          adultQuestion = selectRandomFromArray(questionGroup.filter({ adult: true }));
-          showQuestion(adultQuestion);
-          checkIfWin();
-        } else {
-          adultQuestionCount--;
+    let finished = false;
+    function updater(e: KeyboardEvent) {
+        if (finished) return;
+
+        let adult = false;
+        let option = "";
+
+        if (e.key == "a") {
+            option = selectOption(false, 1);
+        } else if (e.key == "s") {
+            option = selectOption(false, 2);
+        } else if (e.key == "d") {
+            option = selectOption(false, 3);
+        } else if (e.key == "f") {
+            option = selectOption(false, 4);
+        } else if (e.key == "h") {
+            adult = true;
+            option = selectOption(true, 1);
+        } else if (e.key == "j") {
+            adult = true;
+            option = selectOption(true, 2);
+        } else if (e.key == "k") {
+            adult = true;
+            option = selectOption(true, 3);
+        } else if (e.key == "l") {
+            adult = true;
+            option = selectOption(true, 4);
         }
-      } else {
-        if (checkAnswer(option, childQuestion)) {
-          childQuestionCount++;
-          childQuestion = selectRandomFromArray(questionGroup.filter({ adult: false }));
-          showQuestion(childQuestion);
-        } else {
-          childQuestionCount--;
-          checkIfWin();
-        }
-      }
-    }
-  }
 
-  function checkIfWin() {
-    if (adultQuestionCount >= 20) {
-      finished = true;
-      document.querySelector("#winner")!.innerHTML = "The PARENT has WON!"; 
-      document.querySelector("#back-to-home")!.innerHTML = "Back to home!";
-      document.querySelector(".flex")!.remove();
-    } else if (childQuestionCount >= 20) {
-      finished = true;
-      document.querySelector("#winner")!.innerHTML = "The CHILD has WON!"; 
-      document.querySelector("#back-to-home")!.innerHTML = "Back to home!";
-      document.querySelector(".flex")!.remove();
+        if (option != "") {
+            if (adult) {
+                if (checkAnswer(option, adultQuestion)) {
+                    adultQuestionCount++;
+                    adultQuestion = selectRandomFromArray(questionGroup.filter({ adult: true }));
+                    showQuestion(adultQuestion);
+                    checkIfWin();
+                } else {
+                    adultQuestionCount--;
+                }
+            } else {
+                if (checkAnswer(option, childQuestion)) {
+                    childQuestionCount++;
+                    childQuestion = selectRandomFromArray(questionGroup.filter({ adult: false }));
+                    showQuestion(childQuestion);
+                } else {
+                    childQuestionCount--;
+                    checkIfWin();
+                }
+            }
+        }
     }
-  }
-  
-  document.addEventListener("keydown", updater);
+
+    function checkIfWin() {
+        if (adultQuestionCount >= 20) {
+            finished = true;
+            document.querySelector("#winner")!.innerHTML = "The PARENT has WON!";
+            document.querySelector("#back-to-home")!.innerHTML = "Back to home!";
+            document.querySelector(".flex")!.remove();
+        } else if (childQuestionCount >= 20) {
+            finished = true;
+            document.querySelector("#winner")!.innerHTML = "The CHILD has WON!";
+            document.querySelector("#back-to-home")!.innerHTML = "Back to home!";
+            document.querySelector(".flex")!.remove();
+        }
+    }
+
+    document.addEventListener("keydown", updater);
 }
 
 runQuiz();
--- src/readyUpButton.ts
+++ src/readyUpButton.ts
@@ -1,88 +1,89 @@
 export function setupButtons() {
-  const readyBtn: HTMLButtonElement = document.querySelector("#btn-1")!;
-  const upBtn: HTMLButtonElement = document.querySelector("#btn-2")!;
+    const readyBtn: HTMLButtonElement = document.querySelector("#btn-1")!;
+    const upBtn: HTMLButtonElement = document.querySelector("#btn-2")!;
 
-  let ready = false;
-  let up = false;
+    let ready = false;
+    let up = false;
 
-  readyBtn.onclick = () => {
-    readyBtn.disabled = true;
-    readyBtn.childNodes[1].remove();
-    ready = true;
-    if (ready && up) {
-      window.location.href += "quiz/";
-    }
+    readyBtn.onclick = () => {
+        readyBtn.disabled = true;
+        readyBtn.childNodes[1].remove();
+        ready = true;
+        if (ready && up) {
+            window.location.href += "quiz/";
+        }
 
-    if (document.querySelector("child-shop")) {
-      document.querySelector("child-shop")!.remove();
-    }
-  };
-  
-  document.addEventListener("keydown", (e) => {
-    if (e.key == "a") {
-      readyBtn.disabled = true;
-      readyBtn.childNodes[1].remove();
-      ready = true;
+        if (document.querySelector("child-shop")) {
+            document.querySelector("child-shop")!.remove();
+        }
+    };
 
-      if (ready && up) {
-        window.location.href += "quiz/";
-      }
-      
-      if (document.querySelector("child-shop")) {
-        document.querySelector("child-shop")!.remove();
-      }
-    }
-  });
+    document.addEventListener("keydown", (e) => {
+        if (e.key == "a") {
+            readyBtn.disabled = true;
+            readyBtn.childNodes[1].remove();
+            ready = true;
 
-  upBtn.onclick = () => {
-    upBtn.disabled = true;
-    upBtn.childNodes[1].remove();
-    up = true;
+            if (ready && up) {
+                window.location.href += "quiz/";
+            }
 
-    if (ready && up) {
-      window.location.href += "quiz/";
-    }
-    
-    if (document.querySelector("adult-shop")) {
-      document.querySelector("adult-shop")!.remove();
-    }
-  };
-  
-  document.addEventListener("keydown", (e)=> {
-    if (e.key =="l") {
-      upBtn.disabled = true;
-      upBtn.childNodes[1].remove();
-      up = true;
+            if (document.querySelector("child-shop")) {
+                document.querySelector("child-shop")!.remove();
+            }
+        }
+    });
 
-      if (ready && up) {
-        window.location.href += "quiz/";
-      }
-      
-      if (document.querySelector("adult-shop")) {
-        document.querySelector("adult-shop")!.remove();
-      }
-    }
-  });
+    upBtn.onclick = () => {
+        upBtn.disabled = true;
+        upBtn.childNodes[1].remove();
+        up = true;
+
+        if (ready && up) {
+            window.location.href += "quiz/";
+        }
+
+        if (document.querySelector("adult-shop")) {
+            document.querySelector("adult-shop")!.remove();
+        }
+    };
+
+    document.addEventListener("keydown", (e) => {
+        if (e.key == "l") {
+            upBtn.disabled = true;
+            upBtn.childNodes[1].remove();
+            up = true;
+
+            if (ready && up) {
+                window.location.href += "quiz/";
+            }
+
+            if (document.querySelector("adult-shop")) {
+                document.querySelector("adult-shop")!.remove();
+            }
+        }
+    });
 }
 
 export function fadeButtons() {
-  const readyBtn: HTMLButtonElement = document.querySelector("#btn-1")!;
-  const upBtn: HTMLButtonElement = document.querySelector("#btn-2")!;
-  const shopButtons = document.querySelectorAll(".btn-shop")!;
+    const readyBtn: HTMLButtonElement = document.querySelector("#btn-1")!;
+    const upBtn: HTMLButtonElement = document.querySelector("#btn-2")!;
+    const shopButtons: NodeListOf<HTMLButtonElement> = document.querySelectorAll(".btn-shop")!;
 
-  let opacity = 1.0;
-  const fading = setInterval(() => {
-    opacity -= 0.01;
-    
-    readyBtn.style.opacity = opacity.toString();
-    upBtn.style.opacity = opacity.toString(); 
-    shopButtons[0].style.opacity = opacity.toString();
-    shopButtons[1].style.opacity = opacity.toString();
+    let opacity = 1.0;
+    const fading = setInterval(() => {
+        opacity -= 0.01;
 
-    if (opacity <= 0) {
-      clearInterval(fading);
-      readyBtn.remove();
-      upBtn.remove();
-    }
-  }, 10);
+        readyBtn.style.opacity = opacity.toString();
+        upBtn.style.opacity = opacity.toString();
+
+        shopButtons[0].style.opacity = opacity.toString();
+        shopButtons[1].style.opacity = opacity.toString();
+
+        if (opacity <= 0) {
+            clearInterval(fading);
+            readyBtn.remove();
+            upBtn.remove();
+        }
+    }, 10);
 }
